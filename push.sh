935c683b0c73f774ad4372ca171828702ad3fe4f
Dennis Becker <mail.dennisbecker@gmail.com>
2018-05-14 11:09:21 +0200

    fixes issues in push script

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 now=$(date +"%m_%d_%Y")
 
-git add --force .
+git add --all --force .
 git commit --message "Updates guild data - $now"
-git push --quit "https://${GITHUB_TOKEN}@github.com/DennisBecker/bataillon.git" origin master
+git push --quiet "https://${GITHUB_TOKEN}@github.com/DennisBecker/bataillon.git" origin master
