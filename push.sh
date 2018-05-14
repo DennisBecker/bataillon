0f8f7c44d4979453dc6f7da7032f26705d4c9313
Dennis Becker <mail.dennisbecker@gmail.com>
2018-05-14 11:15:44 +0200

    try to fix pushing back to github

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ now=$(date +"%m_%d_%Y")
 
 git add --all --force .
 git commit --message "Updates guild data - $now"
-git push --quiet "https://${GITHUB_TOKEN}@github.com/DennisBecker/bataillon.git" origin master
+git push --quiet "https://${GITHUB_TOKEN}@github.com/DennisBecker/bataillon.git" HEAD:master
