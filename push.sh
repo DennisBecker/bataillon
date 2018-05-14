7960b6e9529ef4934693c60f2b34e1c11f20667e
Dennis Becker <mail.dennisbecker@gmail.com>
2018-05-14 11:03:54 +0200

    use GITHUB_TOKEN for pushing back into repository

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ now=$(date +"%m_%d_%Y")
 
 git add --force .
 git commit --message "Updates guild data - $now"
-git push origin master
+git push --quit "https://${GITHUB_TOKEN}@github.com/DennisBecker/bataillon.git" origin master
